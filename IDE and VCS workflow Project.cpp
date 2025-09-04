33f23e01dc3ac16d0cb473bf4ebed0f82ac81f20
awwpo <awwpo@POOHINSPIRION>
2025-09-03 22:05:57 -0400

    Add project files.

---
+++ IDE and VCS workflow Project.cpp
@@ -0,0 +1,17 @@
+// IDE and VCS workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    char first, middle, last;
+    // Storing initials
+    first = 'T';
+    middle = 'C';
+    last = 'P';
+
+    cout << first << middle << last << endl;
+    return 0;
+}
